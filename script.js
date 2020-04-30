30aebe67ce0a0aadfe6575044825fc307eb2ccca
Anitha Venkatesan <anithamca68@gmail.com>
2020-04-30 15:45:03 -0500

    added past,future,present timing

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ $(document).ready(function() {
   $("#currentDay").text(moment().format('dddd')+","+ moment().format('MMMM Do'));
   var todayTime =moment().format('HH'); 
   console.log(todayTime);
-  var getStorageList =["9","10","11","12","13","14","15","16","17"];
+  var getStorageList =["09","10","11","12","13","14","15","16","17"];
   getStorageList.forEach(element => {
    var getStorage=localStorage.getItem(element);
- 
-   //console.log(getStorage);
    $('#planner-' + element).val(getStorage);
     if(element < todayTime )
     {
       $('#planner-' + element).prop('disabled', true);
+      $('#btn-'+element).prop('disabled',true);
       $('#planner-' + element).addClass('past');
       
     }
@@ -27,28 +26,10 @@ $(document).ready(function() {
     {
       $('#planner-' + element).addClass('future');
     }
-
-    
-
-   
-
-
   });
- 
- 
-  
-
-
   $(".saveBtn").on("click",function(){
     var btnValue = $(this).attr('value');
     var textAreaValue = $('#planner-' + btnValue).val();
-    
-    //console.log(btnValue + "----" + textAreaValue);
     var setStorage = localStorage.setItem(btnValue,textAreaValue);
-  
-  });
-
-  
-      
-   
+  }); 
 });
